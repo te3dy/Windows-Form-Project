5601368957256e3391d0ab5c90c12d7138738460
vutrongtuyenvn <vutrongtuyenvn@gmail.com>
2017-11-07 16:33:52 +0700

    Project
    Tao đổi cái tên

--- Windows Form Project/FormMain.Designer.cs
+++ Windows Form Project/FormMain.Designer.cs
@@ -349,8 +349,9 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Sửa Độc Giả";
+            this.button2.Text = "Done";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
