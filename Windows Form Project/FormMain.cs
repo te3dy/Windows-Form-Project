e7b8ca8f6908010fb1b8a12bd42c43222ea089b5
Teedy <truongto97@gmail.com>
2017-11-09 19:10:25 +0700

    Revert "Merge branch 'master' of https://github.com/te3dy/Windows-Form-Project"
    This reverts commit 3e48b1ed295bd268725bb028756a7a05bf1f3618, reversing
    changes made to 831da7bbcaa5e4bfe21f7b1881e3941c89e7744c.

--- Windows Form Project/FormMain.cs
+++ Windows Form Project/FormMain.cs
@@ -15,45 +15,6 @@ namespace Windows_Form_Project
         public FormMain()
         {
             InitializeComponent();
-     
         }
-<<<<<<< HEAD
-=======
-       
-        private void thêmTàiLiệuToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            /*FormTaoTaiLieu formTaoTaiLieu = new FormTaoTaiLieu();
-            this.Enabled = false;
-            formTaoTaiLieu.Show();*/
-            
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void đăngXuấtToolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            //TODO..............
-
-            DialogResult dig = MessageBox.Show("Đăng xuất thành công ! Bạn có muốn đăng nhập với tài khoản khác không ? ",
-           "Thông báo !", MessageBoxButtons.YesNo);
-            if (dig == DialogResult.No)
-            {
-               this.Close();
-            }
-            else
-            {
-                frmDangNhap f = new frmDangNhap();
-                f.Show();
-            }
-        }
-
-        private void FormMain_Load(object sender, EventArgs e)
-        {
-
-        }
->>>>>>> a1bf89998bddca444e5b4e455d44ec7b4ec1ab02
     }
 }
