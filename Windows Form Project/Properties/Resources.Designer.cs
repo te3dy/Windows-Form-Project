58c29be0c3ba32e26ca1bb3a6018500b5cb5e094
vutrongtuyenvn <vutrongtuyenvn@gmail.com>
2017-11-17 20:58:03 +0700

    Revert "Revert "Merge branch 'teedy' - Hoàn thiện UI""
    This reverts commit 4d9d647d36febb55f4c1e05b51cd2dd6ed08d412.

--- Windows Form Project/Properties/Resources.Designer.cs
+++ Windows Form Project/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Windows_Form_Project.Properties {
+namespace GUI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Windows_Form_Project.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Windows_Form_Project.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
